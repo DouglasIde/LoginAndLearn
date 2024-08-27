270ceb48f036fcd934f1ba906ce15d11f936d7a3
DouglasIde <douglasymide@gmail.com>
2024-08-27 12:15:44 -0300

    Feat: Adding the card with the form to the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent]
+  imports: [HeaderComponent, FooterComponent, CadastroComponent]
 })
 export class AppComponent {
   title = 'login-cadastro-dymi';
