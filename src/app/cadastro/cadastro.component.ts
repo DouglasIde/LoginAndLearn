e7e53acaa9cf67155c76c88022ff39a3138f76f0
DouglasIde <douglasymide@gmail.com>
2024-08-28 11:04:00 -0300

    Fix: Correcting the stepper import

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { InfoPessoalComponent } from "./info-pessoal/info-pessoal.component";
+import { StepperComponent } from "../stepper/stepper.component";
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [InfoPessoalComponent],
+  imports: [InfoPessoalComponent, StepperComponent, CdkStepperModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, NgModule } from '@angular/core';
-import { CdkStepper } from '@angular/cdk/stepper';
+import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-stepper',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CdkStepperModule],
   templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.scss'
+  styleUrl: './stepper.component.scss',
+  providers: [{provide: CdkStepper, useExisting: StepperComponent}]
 })
 export class StepperComponent extends CdkStepper {
   @Input() linearModeSelected = true;
