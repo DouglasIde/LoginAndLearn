f5820aeac74b29d22ec485dffb6e70e368f0b9e5
DouglasIde <douglasymide@gmail.com>
2024-08-28 11:58:51 -0300

    Feat: Creating the address and security components of the user account along with the stepper steps

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { InfoPessoalComponent } from "./info-pessoal/info-pessoal.component";
 import { StepperComponent } from "../stepper/stepper.component";
 import { CdkStepperModule } from '@angular/cdk/stepper';
+import { EnderecoComponent } from "./endereco/endereco.component";
+import { ContaComponent } from "./conta/conta.component";
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [InfoPessoalComponent, StepperComponent, CdkStepperModule],
+  imports: [InfoPessoalComponent, StepperComponent, CdkStepperModule, EnderecoComponent, ContaComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, NgModule } from '@angular/core';
-import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
+import { Component, Input, NgModule, QueryList, TemplateRef, ViewChildren } from '@angular/core';
+import { CdkStep, CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-stepper',
@@ -13,7 +13,18 @@ import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
 export class StepperComponent extends CdkStepper {
   @Input() linearModeSelected = true;
 
+  // @ViewChildren(CdkStep) steps!: QueryList<CdkStep>;
+
   onClick(index: number){
     this.selectedIndex = index;
+  }
+
+  get stepsItems(){
+    return this.steps.toArray();
+  }
+
+  getStepContent(index: number): TemplateRef<any> | null{
+    const step = this.stepsItems[index];
+    return step ? step.content : null;
   }
 }
